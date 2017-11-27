cb9dc134135a96003ac70533ee28d9bcbe2f78f5
Davi Monteiro Barbosa <davimonteiro.ce@gmail.com>
2017-11-26 21:25:06 -0300

    Static files.

---
+++ src/main/resources/static/js/init.js
@@ -0,0 +1,9 @@
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    format: 'dd/mm/yyyy',
+    closeOnSelect: false // Close upon selecting a date,
+});
